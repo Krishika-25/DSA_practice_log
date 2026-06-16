b3f6feae4c000fb875ef606b712c8a35e708d4a2
Krishika25 <krishikad25@gmail.com>
2026-06-17 01:13:00 +0530

    Solved Majority Element (n/2) and Majority Element II (n/3) using Boyer-Moore Voting Algorithm

---
+++ Boyer_Moore_Algorithm/Majority_element_II.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> majorityElement(vector<int>& nums) {
+    int candidate1 = 0, candidate2 = 0;
+    int count1 = 0, count2 = 0;
+
+    for (int x : nums) {
+        if (x == candidate1) {
+            count1++;
+        }
+        else if (x == candidate2) {
+            count2++;
+        }
+        else if (count1 == 0) {
+            candidate1 = x;
+            count1 = 1;
+        }
+        else if (count2 == 0) {
+            candidate2 = x;
+            count2 = 1;
+        }
+        else {
+            count1--;
+            count2--;
+        }
+    }
+
+    count1 = 0;
+    count2 = 0;
+
+    for (int x : nums) {
+        if (x == candidate1)
+            count1++;
+        else if (x == candidate2)
+            count2++;
+    }
+
+    vector<int> ans;
+
+    if (count1 > nums.size() / 3)
+        ans.push_back(candidate1);
+
+    if (count2 > nums.size() / 3)
+        ans.push_back(candidate2);
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {3, 2, 3};
+
+    vector<int> ans = majorityElement(nums);
+
+    cout << "Majority elements: ";
+    for (int x : ans) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
